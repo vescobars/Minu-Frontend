cd56a6631f7be50abba05c0a675650ed0d6ff154
veronica <v.escobar1@uniandes.edu.co>
2022-11-27 23:59:31 -0500

    Ajustes finales

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -5,31 +5,36 @@ import OrderCard from "./OrderCard";
 
 
 export default function Orders(props) {
-    const[data, setData] = useState({
-        orderId: "",
-        orderState: "",
-        orderDate: "",
-        orderPrice: "",
+    const [data, setData] = useState({
+        name: "",
     });
 
     const [orders, setOrders] = useState([]);
 
-    useEffect(
-        () => {
-            const URL = "http://localhost:3000/orders";
-            fetch(URL)
-                .then((data) => data.json())
-                .then((data) => {
-                    setOrders(data);
-                });
-        },
-        []
-    );
-
     const handleChange = (e) => {
-        setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+        setOrders((prev) => ({ ...prev, [e.target.name]: e.target.value }));
     };
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
+        const orderInfo = {
+            orderId: orders.orderId,
+            orderState: orders.orderState,
+            orderDate: orders.orderDate,
+            orderPrice: orders.orderPrice,
+        }
+
+        fetch("/api/v1/orders", {
+            method: "POST",
+            headers: {
+                'Content-type': "application/json"
+            },
+            body: JSON.stringify(orderInfo)
+        })
+    }
+
+    
     return(
         <div>
             <div className="d-flex flex-column justify-content-center align-items-center w-100">
--- src/components/QrReader.jsx
+++ src/components/QrReader.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { QrReader } from 'react-qr-reader';
+import { FormattedMessage } from "react-intl";
 
 
 
 const QrCodeReader = () =>{
     const[data,setQrscan] = useState('No result');
 
+    //En teoria esto deberia sacar el link que escanea el qr y lo 
+    //guarda en data para que asi salga la URL para que el usuario 
+    //pueda abrir la pagina del restaurante
     const handleScan = data => {
         if (data) {
             setQrscan(data);
@@ -22,7 +26,7 @@ const QrCodeReader = () =>{
             <center>
                 <div style={{marginTop:100}}>
                     <span className="h2 my-0 text-black text-decoration-none">
-                        QR Menu Scanner
+                        <FormattedMessage id="QR Menu Scanner"/>
                     </span>
                     <QrReader style={{
                         margin: '0',
@@ -34,6 +38,7 @@ const QrCodeReader = () =>{
                         style={{ width: '50%' }}
                     </QrReader>
                 </div>
+                <p>{data}</p>
             </center>
         </div>
     );
