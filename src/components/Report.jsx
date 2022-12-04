d90ce99b20c083430866eb8edd22d653602410a6
nicolas <sn.rodriguezc@uniandes.edu.co>
2022-12-04 03:18:34 -0500

    pwa nicolas

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,28 +1,38 @@
 import React from "react";
-import OrderCard from "./OrderCard";
-import ReviewCard from "./ReviewCard";
+import Card from 'react-bootstrap/Card';
 import { useEffect, useState } from "react";
 
 import { FormattedMessage } from "react-intl";
 
 import ReportDetail from "./ReportDetail";
+import { Row, Col } from "react-bootstrap";
 
 function Report() {
   const [restaurants, setRestaurants] = useState([]);
   const [selectedRest, setSelectedRest] = useState([]);
 
   useEffect(() => {
-    console.log("here")
-    const URL = "/api/v1/sites";
-    fetch(URL)
-      .then((data) => data.json())
-      .then((data) => {
+    console.log(restaurants)
+    if(!navigator.onLine){
+      if(localStorage.getItem("restaurants") === null){
+        setRestaurants([])
+      } else {
+        setRestaurants(localStorage.getItem("restaurants"));
+      }
+    } else {
+      const URL = "http://localhost:3000/api/v1/sites";
+      fetch(URL).then((data) => data.json()).then((data) => {
         setRestaurants(data);
-      });
+        localStorage.setItem("restaurants", data);
+      }) 
+    }
   }, [selectedRest]);
 
+  if (restaurants.length == 0){
+    return null
+  }
+
   const selectDetail = (restaurant) => {
-    console.log(restaurant)
     setSelectedRest(restaurant)
   }
 
@@ -56,112 +66,20 @@ function Report() {
               <option value={res.id}>{res.description}</option>
             ))}
         </select> */}
-        <ol>
-          {restaurants.map(res => (
-              <li onClick={selectDetail.bind(this, res)}>{res.description}</li>
+        <div style={{overflowX: "auto"}}>
+          <Row>
+          {restaurants?.map(res => (
+              <Card style={{cursor: "pointer", width:"auto"}} onClick={selectDetail.bind(this, res)}>
+                <Card.Body>
+                  <Card.Title >{res.description}</Card.Title>
+                </Card.Body>
+              </Card>
             ))}
-        </ol>
+          </Row>
+        </div>
       </div>
       <hr></hr>
-
-      <div id="Ordenes">
-        <h2>
-          <FormattedMessage id="Orders"/>
-        </h2>
-        <div 
-          style={{
-            display: "grid",
-            gridTemplateColumns: "33% 33% 33%",
-            margin: "0",
-            fontSize:"110%",
-            textAlign: "center",
-          }}>
-          <div >
-            <FormattedMessage id="OrdersTotal"/> 10
-          </div>
-          <div >
-            <FormattedMessage id="TotalPrice"/>$99'999.999
-          </div>
-          <div >
-            <FormattedMessage id="Since">{placeholder=>  
-              <input style={{width:"100px"}} placeholder={placeholder}/>}
-            </FormattedMessage>
-            <FormattedMessage id="Until">{placeholder=>  
-              <input style={{width:"100px"}} placeholder={placeholder}/>}
-            </FormattedMessage>
-            <button className="border-0 rounded py-1 px-2 text-white" style={{background: "#6A1818"}}>
-              <FormattedMessage id="Apply"/>
-            </button>
-          </div>
-        </div>
-        <br></br>
-
-        <div 
-          style={{
-            height: "160px",
-            overflowX: "auto",
-          }}>
-          <div style={{ width:"max-content"}}>
-            <ReportDetail Restaurant={selectedRest} />
-            {/* {orders?.map((order, i) => (
-              <OrderCard id={i} state={order.state} date={order.date} totalValue={order.totalValue}/>
-            ))} */}
-            {/* <OrderCard id="#1" state="Done" date="2019-03-26"/>
-            <OrderCard id="#2" state="Done" date="2019-03-26"/>
-            <OrderCard id="#3" state="Done" date="2019-03-26"/>
-            <OrderCard id="#4" state="Done" date="2019-03-26"/>
-            <OrderCard id="#5" state="Done" date="2019-03-26"/>
-            <OrderCard id="#6" state="Done" date="2019-03-26"/>
-            <OrderCard id="#7" state="Done" date="2019-03-26"/>
-            <OrderCard id="#8" state="Done" date="2019-03-26"/>
-            <OrderCard id="#9" state="Done" date="2019-03-26"/>
-            <OrderCard id="#10" state="Done" date="2019-03-26"/>
-            <OrderCard id="#11" state="Done" date="2019-03-26"/> */}
-          </div>
-        </div>
-      </div>
-
-      <div id="Reviews">
-        <h2>
-          <FormattedMessage id="Reviews"/>
-        </h2>
-        <div 
-          style={{
-            height: "240px",
-            overflowX: "auto",
-          }}>
-          <div style={{ width:"max-content"}}>
-            <ReviewCard score="1" client="Juan" description="Curabitur ut neque sed magna pharetra fringilla vel a ante.
-                Suspendisse id suscipit mauris, et egestas lacus. Pellentesque
-                habitant morbi tristique senectus et netus et malesuada fames ac
-                turpis egestas."/>
-            <ReviewCard score="2" client="Juan" description="Curabitur ut neque sed magna pharetra fringilla vel a ante.
-                Suspendisse id suscipit mauris, et egestas lacus. Pellentesque
-                habitant morbi tristique senectus et netus et malesuada fames ac
-                turpis egestas."/>
-            <ReviewCard score="3" client="Juan" description="Curabitur ut neque sed magna pharetra fringilla vel a ante.
-                Suspendisse id suscipit mauris, et egestas lacus. Pellentesque
-                habitant morbi tristique senectus et netus et malesuada fames ac
-                turpis egestas."/>
-            <ReviewCard score="4" client="Juan" description="Curabitur ut neque sed magna pharetra fringilla vel a ante.
-                Suspendisse id suscipit mauris, et egestas lacus. Pellentesque
-                habitant morbi tristique senectus et netus et malesuada fames ac
-                turpis egestas."/>
-            <ReviewCard score="5" client="Juan" description="Curabitur ut neque sed magna pharetra fringilla vel a ante.
-                Suspendisse id suscipit mauris, et egestas lacus. Pellentesque
-                habitant morbi tristique senectus et netus et malesuada fames ac
-                turpis egestas."/>
-            <ReviewCard score="5" client="Juan" description="Curabitur ut neque sed magna pharetra fringilla vel a ante.
-                Suspendisse id suscipit mauris, et egestas lacus. Pellentesque
-                habitant morbi tristique senectus et netus et malesuada fames ac
-                turpis egestas."/>
-          </div>
-        </div>
-
-        <button className="mt-2 md-2 border-0 rounded py-2 px-3 text-white" style={{background: "#6A1818"}}>
-          <FormattedMessage id="DownloadReport"/>
-        </button>
-      </div>
+      <ReportDetail Restaurant={selectedRest} />
     </>
   );
 }
--- src/components/ReportDetail.jsx
+++ src/components/ReportDetail.jsx
@@ -1,31 +1,113 @@
 import { useEffect, useState } from "react";
 import OrderCard from "./OrderCard";
+import ReviewCard from "./ReviewCard";
+import { FormattedMessage } from "react-intl";
 
 function ReportDetail(props) {
     const [orders, setOrders] = useState([]);
+    const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
-        console.log("rd")
-        if (typeof props.Restaurant.id !== 'undefined'){
-            
-            const ordersURL = "/api/v1/sites/"+props.Restaurant.id+"/orders";
-            fetch(ordersURL)
-            .then((data) => data.json())
-            .then((data) => {
-            setOrders(data);
-            });
-        }  
+        if(!navigator.onLine){
+            if(localStorage.getItem("orders") === null){
+                setOrders("Loading...")
+            } else {
+                setOrders(localStorage.getItem("orders"));
+            }
+        } else {
+            if (typeof props.Restaurant.id !== 'undefined'){
+                const ordersURL = "http://localhost:3000/api/v1/sites/"+props.Restaurant.id+"/orders";
+                fetch(ordersURL).then((data) => data.json()).then((data) => {
+                    setOrders(data);
+                    localStorage.setItem("orders", data);
+                });
+            }
+        }
+    }, [props]);
+
+    useEffect(() => {
+        if(!navigator.onLine){
+            if(localStorage.getItem("reviews") === null){
+                setReviews("Loading...")
+            } else {
+                setReviews(localStorage.getItem("reviews"));
+            }
+        } else {
+            if (typeof props.Restaurant.id !== 'undefined'){
+                const reviewsURL = "http://localhost:3000/api/v1/sites/"+props.Restaurant.id+"/reviews";
+                fetch(reviewsURL).then((data) => data.json()).then((data) => {
+                    setReviews(data);
+                    localStorage.setItem("reviews", data);
+                });
+            }
+        }
     }, [props]);
 
     if (typeof props.Restaurant.id == 'undefined'){
         return null
     }
+
+    const getTotalValue = () => {
+        let total = 0
+        for (let i = 0; i < orders.length; i += 1){
+            total += orders[i].totalValue
+        }
+        return total
+    }
         
     return(
         <div>
-            {orders?.map((order, i) => (
-                <OrderCard id={i} state={order.state} date={order.date} totalValue={order.totalValue}/>
-          ))}
+            <h2>
+                <FormattedMessage id="Orders"/>
+            </h2>
+            <div 
+                style={{
+                    display: "grid",
+                    gridTemplateColumns: "33% 33% 33%",
+                    margin: "0",
+                    fontSize:"110%",
+                    textAlign: "center",
+                }}>
+                <div>
+                    <FormattedMessage id="OrdersTotal"/> {orders?orders.length:0}
+                </div>
+                <div>
+                    <FormattedMessage id="TotalPrice"/> ${orders?getTotalValue():0}
+                </div>
+                <div>
+                    <FormattedMessage id="Since">{placeholder=>  
+                    <input style={{width:"100px"}} placeholder={placeholder}/>}
+                    </FormattedMessage>
+                    <FormattedMessage id="Until">{placeholder=>  
+                    <input style={{width:"100px"}} placeholder={placeholder}/>}
+                    </FormattedMessage>
+                    <button className="border-0 rounded py-1 px-2 text-white" style={{background: "#6A1818"}}>
+                    <FormattedMessage id="Apply"/>
+                    </button>
+                </div>
+            </div>
+            <div style={{height: "160px",overflowX: "auto"}}>
+                <div style={{ width:"max-content"}}>
+                    {orders?.map((order, i) => (
+                        <OrderCard id={i} state={order.state} date={order.date} totalValue={order.totalValue}/>
+                    ))}
+                </div>
+            </div>
+                        
+            <div style={{height: "240px",overflowX: "auto"}}>
+                <h2>
+                    <FormattedMessage id="Reviews"/>
+                </h2>
+                <div style={{ width:"max-content"}}>
+                    {reviews?.map((review, i) => (
+                        <ReviewCard id={i} score={review.score} description={review.description}/>
+                    ))}        
+                </div>
+            </div>
+
+            <button className="mt-2 md-2 border-0 rounded py-2 px-3 text-white" style={{background: "#6A1818"}}>
+                <FormattedMessage id="DownloadReport"/>
+            </button>
         </div>
     )
 }
